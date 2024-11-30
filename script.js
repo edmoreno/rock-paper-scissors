fa5914499e9b5cb586341ad25009e16d363157a4
Eduardo Moreno <48771676+edmoreno@users.noreply.github.com>
2024-11-29 23:09:21 -0500

    added playGame loop to play 5 rounds, added ties

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 
-let humanScore = 0;
-let computerScore = 0;
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
 function getComputerChoice() {
     let randomVal = Math.random() * 100;
     if (randomVal <= 33) {
@@ -31,22 +23,28 @@ function playRound(humanSelection, computerSelection) {
         // wins if computer gives rock
         if (computerChoice === 'rock') {
             console.log("You win! Paper beats Rock");
-            humanScore++;
+            return "human";
         } else if (computerChoice === 'scissors') {
         // loses if computer gives scissors
             console.log("You lose! Scissors beats Rock");
-            computerScore++;
+            return "computer";
+        } else {
+            console.log("You tied!");
+            return "tie";
         }
     // if human gives rock:
     } else if (humanChoice === 'rock') {
         // wins if computer gives scissors
         if (computerChoice === 'scissors') {
             console.log("You win! Rock beats Scissors");
-            humanScore++;
+            return "human";
         // loses if computer gives paper
         } else if (computerChoice === 'paper') {
             console.log("You lose! Paper beats Rock");
-            computerScore++;
+            return "computer";
+        } else {
+            console.log("You tied!");
+            return "tie";
         }
     // if human gives scissors:
     } else if (humanChoice === 'scissors') {
@@ -54,13 +52,40 @@ function playRound(humanSelection, computerSelection) {
         // loses if computer gives rock
         if (computerChoice === 'paper') {
             console.log('You win! Scissors beats Paper');
-            humanScore++;
+            return "human";
         } else if (computerChoice === 'rock') {
             console.log('You lose! Rock beats Scissors');
-            computerScore++;
+            return "computer";
+        } else{
+            console.log("You tied!");
+            return "tie";
         }
     }
 }
 
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    for (let i = 0; i < 5; ) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
 
+        let winner = playRound(humanSelection, computerSelection);
+        if (winner === 'human') {
+            humanScore++;
+            i++;
+        } else if (winner === 'computer') {
+            computerScore++;
+            i++;
+        }
+    }
 
+    if (humanScore > computerScore) {
+        console.log("You win!");
+    } else {
+        console.log("Computer wins!");
+    }
+}
+
+playGame();
